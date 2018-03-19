9e24cd76d69eb0f83836642ffd305a595d88a298
Ana Avilés <ana.aviles@atos.net>
2018-03-19 11:59:09 +0100

    pantalla correo

---
+++ pantalla-correo/src/main/java/pantalla/correo/constants/pantallaCorreoKeys.java
@@ -0,0 +1,14 @@
+package pantalla.correo.constants;//my.npm.angular.portlet.constants;
+
+/**
+ * @author a599898
+ */
+
+ //??
+
+ 
+public class pantallaCorreoKeys {
+
+	public static final String MyNpmAngular = "MyNpmAngular";
+
+}
---
+++ pantalla-correo/src/main/java/pantalla/correo/portlet/pantallaCorreo.java
@@ -0,0 +1,32 @@
+package pantalla.correo.portlet;//my.npm.angular.portlet.portlet;
+
+import pantalla.correo.constants.pantallaCorreoKeys;
+//my.npm.angular.portlet.constants.MyNpmAngularPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author a599898
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=pantalla-correo Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=pantallaCorreoKeys.MyNpmAngular",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+
+//??
+public class pantallaCorreo extends MVCPortlet {
+}
